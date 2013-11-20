19aca579db93042e6b88bee62cd12443939f4e56
fabsal <fabricio.salinas@gmail.com>
2013-11-20 17:14:03 -0600

    div content-main con height automatico

--- skeletontx/Scripts/ternium/layout.js
+++ skeletontx/Scripts/ternium/layout.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
-    //Muestra o no el encabezado
+    // redefine el tamaño del contenido
+    $('.content-main').css('height', $(window).height() - $('.content-header').height() - $('.content-footer').height() - 50); // el 50 es el padding-top del body; usar media queries
+
+    // define acciones para la animación de la toolbar
     $(document).on("click", ".contenedorFiltroColapsable", function () {
-        $(this).parent().children('.contenidoFiltros').slideToggle();
+        // despliega-colapsa toolbar
+        $(this).parent().children('.contenidoFiltros').slideToggle("slow", function() {
+            // redefine el tamaño del contenido
+            $('.content-main').css('height', $(window).height() - $('.content-header').height() - $('.content-footer').height() - 50); // el 50 es el padding-top del body; usar media queries
+        });
     });
+
+    // define acciones para el windows resize
+    window.onresize = function() {
+        $('.content-main').css('height', $(window).height() - $('.content-header').height() - $('.content-footer').height() - 50); // el 50 es el padding-top del body; usar media queries
+    };
 });
